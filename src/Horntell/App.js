ad496e0babad81cff0123826df13761de545a618
Mayank Thapliyal <mayankthapliyal99@gmail.com>
2015-03-31 16:35:35 +0530

    App resource added and README.md altered

---
+++ src/Horntell/App.js
@@ -0,0 +1,68 @@
+'use strict';
+
+function App() {
+
+	if( ! (this instanceof App))
+		return new App();
+
+	this.base = 'https://api.horntell.com';
+	this.version = 'v1';
+	this.key = null;
+	this.secret = null;
+}
+
+App.prototype = {
+
+	init: function(key, secret) {
+		this.setKey(key);
+		this.setSecret(secret);
+	},
+
+	setBase: function(base) {
+		if(base) {
+			this._setAppField('base', base);
+		}
+	},
+
+	setVersion: function(version) {
+		if(version) {
+			this._setAppField('version', version);
+		}
+	},
+
+	setKey: function(key) {
+		if(key)
+			this._setAppField('key', key);
+	},
+
+	setSecret: function(secret) {
+		if(secret)
+			this._setAppField('secret', secret);
+	},
+
+	getBase: function() {
+		return this._getAppField('base');
+	},
+
+	getVersion: function() {
+		return this._getAppField('version');
+	},
+
+	getKey: function() {
+		return this._getAppField('key');
+	},
+
+	getSecret: function() {
+		return this._getAppField('secret');
+	},
+
+	_setAppField: function(key, value) {
+		this[key] = value;
+	},
+
+	_getAppField: function(key) {
+		return this[key];
+	}
+}
+
+module.exports = App;
